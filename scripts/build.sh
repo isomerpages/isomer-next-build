34d97eb301e10fa5618621e863e2a6a27ab10523
Harish V <harish@open.gov.sg>
2024-02-12 17:38:43 +0800

    feat: add build script

---
+++ scripts/build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo "Build started..."
+
+#######################################################################
+# Download package.json and package-lock.json files from central repo #
+#######################################################################
+
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-base-template/main/package.json -o package.json
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-base-template/main/package-lock.json -o package-lock.json
+
+#######################
+# Install NPM modules #
+#######################
+
+npm install
+
+#######################################################################
+# Generate sitemap.json and search index #
+#######################################################################
+mkdir -p src/scripts/
+
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-sitemap.js -o src/scripts/generate-sitemap.js
+curl https://raw.githubusercontent.com/isomerpages/isomer-next-build/main/scripts/generate-search-index.js -o src/scripts/generate-search-index.js
+
+node src/scripts/generate-sitemap.js
+
+echo "Sitemap generated"
+
+node src/scripts/generate-search-index.js
+
+echo "Search index generated"
+
+#######################################################################
+# Copy to public folder #
+#######################################################################
+
+cp -v src/sitemap.json public/
+cp -v src/searchIndex.json public/
+
+echo "Copied sitemap and search index to public folder"
+
+
